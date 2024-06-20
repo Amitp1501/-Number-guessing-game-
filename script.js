744b08672f3213e6749e1edc384c64080535f740
Amit Pandey <amitpandey15012001@gmail.com>
2024-06-20 21:14:03 +0530

    second commit

--- script.js
+++ script.js
@@ -16,54 +16,54 @@ console.log(scretNum);
 // event to check the hide num
 
 btnCheckEl.addEventListener("click", () => {
-    const guess = Number(InputNumEl.value);
-    console.log(guess);
+  const guess = Number(InputNumEl.value);
+  console.log(guess);
 
-    //  check empty input
-    if (guess) {
-        // not match hide Number
-        if (guess != scretNum) {
-            if (Score > 1) {
-                Score--;
-                ScoresEl.textContent = Score;
+  //  check empty input
+  if (guess) {
+    // not match hide Number
+    if (guess != scretNum) {
+      if (Score > 1) {
+        Score--;
+        ScoresEl.textContent = Score;
 
-                MsgEl.textContent =
-                    guess > scretNum ? "You Number High ☝️☝️" : "You Number Low 👇👇";
+        MsgEl.textContent =
+          guess > scretNum ? "You Number High ☝️☝️" : "You Number Low 👇👇";
 
-                ScoresEl.textContent = Score;
-            } else {
-                displayMassage("🤦‍♂️😔You lossed the game 🤦‍♂️😔");
-                containerEl.style.backgroundColor = "#fff";
-                ScoresEl.textContent = 0;
-            }
-        } else {
-            // Success
-            hideNumEl.textContent = scretNum;
-            hideNumEl.style.width = "50%";
-            hideNumEl.style.transition = "all 0.5s ease-in";
-            containerEl.style.backgroundColor = "#e0d8d3";
-            displayMassage("🏆🏆Congratulation You are Won the Game 🏆🏆");
-        }
+        ScoresEl.textContent = Score;
+      } else {
+        displayMassage("🤦‍♂️😔You lossed the game 🤦‍♂️😔");
+        containerEl.style.backgroundColor = "#fff";
+        ScoresEl.textContent = 0;
+      }
     } else {
-        displayMassage("Please Enter the Number :(");
+      // Success
+      hideNumEl.textContent = scretNum;
+      hideNumEl.style.width = "50%";
+      hideNumEl.style.transition = "all 0.5s ease-in";
+      containerEl.style.backgroundColor = "#e0d8d3";
+      displayMassage("🏆🏆Congratulation You are Won the Game 🏆🏆");
     }
+  } else {
+    displayMassage("Please Enter the Number :(");
+  }
 });
 
 // display Massage
 const displayMassage = function (massage) {
-    MsgEl.textContent = massage;
+  MsgEl.textContent = massage;
 };
 
-// Reset the Game 
+// Reset the Game
 btnPlayEl.addEventListener("click", () => {
-    Score = 20;
-    scretNum = Math.floor(Math.random() * 20 + 1);
-    ScoresEl.textContent = Score;
-    hideNumEl.textContent = "?";
-    hideNumEl.style.width = "50%";
-    hideNumEl.style.transition = "all 0.5s ease-in";
-    InputNumEl.value = "";
-    containerEl.style.backgroundColor = "#ddd";
+  Score = 20;
+  scretNum = Math.floor(Math.random() * 20 + 1);
+  ScoresEl.textContent = Score;
+  hideNumEl.textContent = "?";
+  hideNumEl.style.width = "50%";
+  hideNumEl.style.transition = "all 0.5s ease-in";
+  InputNumEl.value = "";
+  containerEl.style.backgroundColor = "#ddd";
 
-    displayMassage("Start Guesing ............");
+  displayMassage("Start Guesing ............");
 });
